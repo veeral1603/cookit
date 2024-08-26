d00adbbc6990dddbcbf13d1c9e4fc0c4bb1d15cb
veeral1603 <veeralnarang999@gmail.com>
2024-08-26 22:50:07 +0530

    Updated filters

--- js/recipes.js
+++ js/recipes.js
@@ -6,8 +6,30 @@ const filterBtn = document.querySelector("[data-filterBtn]");
 const filtersContainer = document.querySelector("[data-filters]");
 
 const openFilters = function(){
-    filtersContainer.classList.toggle("active");
-    console.log("hi")
+    filtersContainer.classList.add("active");
 };
 
 filterBtn.addEventListener("click" , openFilters)
+
+// Mobile Filter Close Button 
+
+const filterCloseBtn = document.querySelector("[data-filterCloseBtn]");
+
+const closeFilters = function() {
+    filtersContainer.classList.remove("active");
+}
+
+filterCloseBtn.addEventListener("click" , closeFilters);
+
+
+// Mobile Filters  
+
+const filterRows = document.querySelectorAll(".filter-row");
+
+filterRows.forEach((row) => {
+    row.addEventListener("click" , function(e){
+        
+        row.querySelector(".filter-content").classList.toggle("active");
+        row.querySelector(".icon").classList.toggle("active");
+    });
+});
