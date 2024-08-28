aa8dc79da9c90a7ac33305ef42b7989a0f33e94a
veeral1603 <veeralnarang999@gmail.com>
2024-08-29 04:56:06 +0530

    Updated Search Filters

--- js/recipes.js
+++ js/recipes.js
@@ -6,30 +6,31 @@ const filterBtn = document.querySelector("[data-filterBtn]");
 const filtersContainer = document.querySelector("[data-filters]");
 
 const openFilters = function(){
-    filtersContainer.classList.add("active");
+    filtersContainer.classList.toggle("active");
 };
 
 filterBtn.addEventListener("click" , openFilters)
 
 // Mobile Filter Close Button 
 
-const filterCloseBtn = document.querySelector("[data-filterCloseBtn]");
+// const filterCloseBtn = document.querySelector("[data-filterCloseBtn]");
 
 const closeFilters = function() {
     filtersContainer.classList.remove("active");
 }
 
-filterCloseBtn.addEventListener("click" , closeFilters);
+// filterCloseBtn.addEventListener("click" , closeFilters);
 
 
 // Mobile Filters  
 
-const filterRows = document.querySelectorAll(".filter-row");
+const filterRowsTitle = document.querySelectorAll(".filter-title-container");
 
-filterRows.forEach((row) => {
+filterRowsTitle.forEach((row) => {
+
     row.addEventListener("click" , function(e){
-        
-        row.querySelector(".filter-content").classList.toggle("active");
-        row.querySelector(".icon").classList.toggle("active");
+    
+        row.closest(".filter-row").querySelector(".filter-content").classList.toggle("active");
+        row.closest(".filter-row").querySelector(".icon").classList.toggle("active");
     });
 });
