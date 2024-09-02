4b0944471297b1c66e76ca07475ebf5765946c8c
veeral1603 <veeralnarang999@gmail.com>
2024-09-02 20:36:43 +0530

    updated media queries

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use script';
+"use script";
 
 import * as route from "./route.js";
 import * as api from "./api.js";
@@ -6,66 +6,85 @@ import * as home from "./home.js";
 import * as recipes from "./recipes.js";
 import * as details from "./details.js";
 
-
-// Mobile Navigation 
+// Navigation
 
 const mobileNavContainer = document.querySelector(".mobile-nav");
 const mobileNavBtns = document.querySelectorAll(".mobile-nav-list li");
 const contentContainers = document.querySelectorAll("article.container");
+const headerTabsContainer = document.querySelector(".header-tabs");
+const headerBookmarkBtn = document.querySelector("[data-header-bookmark]");
 
+// Mobile
+mobileNavContainer.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("mobile-nav-list") ||
+    e.target.classList.contains("mobile-nav")
+  )
+    return;
 
+  const tab = `${e.target
+    .closest(".list-item")
+    .querySelector(".nav-label")
+    .innerHTML.toLowerCase()}`;
 
-mobileNavContainer.addEventListener("click" , function(e){
-    
-    if (e.target.classList.contains("mobile-nav-list") || e.target.classList.contains("mobile-nav")) return;
-
-    const tab = `${e.target.closest(".list-item").querySelector(".nav-label").innerHTML.toLowerCase()}`;
-
-    recipes.closeFiltersRows();
-    recipes.closeFilters();
-    route.changeNavTo(tab);
-
+  recipes.closeFiltersRows();
+  recipes.closeFilters();
+  route.changeNavTo(tab);
 });
 
-// Adjust Footer 
+//PC
+
+headerTabsContainer.addEventListener("click", function (e) {
+  if (!e.target.classList.contains("header-tabs-item")) return;
+
+  const tab = `${e.target.innerHTML.toLowerCase()}`;
+
+  headerTabsContainer.querySelectorAll(".header-tabs-item").forEach((li) => {
+    li.classList.remove("active");
+  });
+
+  e.target.classList.add("active");
+
+  route.changeNavTo(tab);
+});
+
+headerBookmarkBtn.addEventListener("click", function () {
+  headerTabsContainer.querySelectorAll(".header-tabs-item").forEach((li) => {
+    li.classList.remove("active");
+  });
+  route.changeNavTo("saved");
+});
+
+// Adjust Footer
 
 const footer = document.querySelector(".footer");
 const body = document.querySelector("body");
 
-export const adjustFooter = function(){
-    if ( body.offsetHeight <  window.innerHeight)
-    {
-        footer.classList.add("sticky");
-        footer.style.bottom = `${mobileNavContainer.offsetHeight}px`;
-    }
-    else {
-        footer.classList.remove("sticky");
-    }
+export const adjustFooter = function () {
+  if (body.offsetHeight < window.innerHeight) {
+    footer.classList.add("sticky");
+    footer.style.bottom = `${mobileNavContainer.offsetHeight}px`;
+  } else {
+    footer.classList.remove("sticky");
+  }
 };
 
-// Scroll to top 
+// Scroll to top
 
-export const scrollToTop = function(){
-    window.scrollTo(0, 0);
+export const scrollToTop = function () {
+  window.scrollTo(0, 0);
 };
 
+// Recipe details page
 
-// Recipe details page 
+const openDetails = function () {};
 
-const openDetails = function(){
-
-};
-
-
-// Error 404 
+// Error 404
 
 export const error404 = function () {
-    console.log("Error 404");
+  console.log("Error 404");
 };
 
+// Render  Recipes
 
-// Render  Recipes 
-
-export const renderRecipesResults = function(){
-
-};
+export const renderRecipesResults = function () {};
